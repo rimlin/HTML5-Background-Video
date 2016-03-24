478bc6beda79ed4b09a886a236a89e7f82dc99a9
rimlin <ilmirw@gmail.com>
2016-03-24 13:54:42 +0000

    Removed examples, renamed min to index

--- example/js/background-video.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  var videobackground = new $.backgroundVideo('$(body)', {
-    "width": 1280,
-    "height": 720,
-    "path": "media/",
-    "filename": "cloud",
-    "types": ["mp4","ogg","webm"]
-  });
-});
